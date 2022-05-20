d1522572e399e5c3a3e9333df68203d8b4ef1a6d
Noah Sulek <100804236+Noahdamaster@users.noreply.github.com>
2022-05-19 20:12:44 -0400

    Add files via upload

---
+++ valentineGreeting.py
@@ -0,0 +1,11 @@
+##<Noah Sulek>
+##<valentineGreeting>
+
+from graphics import *
+import time
+def valentineGreeting():
+    winWid = 1000
+    winHt = 1000
+    win = GraphWin("Shapes", winWid, winHt)
+
+
